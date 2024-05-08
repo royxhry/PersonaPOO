290a2f9be4cbc64e1464ac0a8c0daf8d719a334e
Rodrigo <hl>
2024-05-08 00:52:10 -0600

    v3

--- MainPersona.java
+++ MainPersona.java
@@ -1,30 +1,44 @@
 import java.util.Scanner;
+
 public class MainPersona {
     public static void main(String[] args) {
+        // Crear un objeto Scanner para leer la entrada del usuario
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingresa nombre y apellido:");
-            String nombre = sc.nextLine();
-            String apellido = sc.nextLine();
-            System.out.println("Ingresa edad:");
-            int edad = sc.nextInt();
-            
-            System.out.println("Ingresa tu altura:");
-            double altura = sc.nextDouble();
-            Persona ps= new Persona();
-            ps.setNombre(nombre);
-            ps.setApellido(apellido);
-            ps.setEdad(edad);
-            ps.setAltura(altura);
-
-            ps.getNombre();
-            ps.getApellido();
-            ps.getEdad();
-            ps.getEdad();
-            System.out.println(ps.toString());
-
-            sc.close();
-
-    }
-
         
+        // Solicitar al usuario que ingrese nombre y apellido
+        System.out.println("Ingresa nombre y apellido:");
+        String nombre = sc.nextLine();
+        String apellido = sc.nextLine();
+        
+        // Solicitar al usuario que ingrese la edad
+        System.out.println("Ingresa edad:");
+        int edad = sc.nextInt();
+        
+        // Solicitar al usuario que ingrese la altura
+        System.out.println("Ingresa tu altura:");
+        double altura = sc.nextDouble();
+        
+        // Crear un objeto de tipo Persona
+        Persona ps= new Persona();
+        
+        // Establecer los valores de nombre, apellido, edad y altura utilizando los métodos set
+        ps.setNombre(nombre);
+        ps.setApellido(apellido);
+        ps.setEdad(edad);
+        ps.setAltura(altura);
+        
+       Persona ps2 = new Persona();
+        ps2.getNombre();
+        ps2.getApellido();
+        ps2.getEdad();
+        ps2.getEdad();
+        
+        // Imprimir la información de la persona utilizando el método toString
+        System.out.println(ps.toString());
+        
+        
+        
+        // Cerrar el Scanner
+        sc.close();
+    }
 }
--- Persona.java
+++ Persona.java
@@ -1,38 +1,52 @@
-
 public class Persona {
+  
     String nombre, apellido;
     int edad;
     double altura;
+    
+    // Método para obtener el nombre de la persona
     public String getNombre() {
         return nombre;
     }
+    
+    // Método para establecer el nombre de la persona
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+    
+    // Método para obtener el apellido de la persona
     public String getApellido() {
         return apellido;
     }
+    
+    // Método para establecer el apellido de la persona
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
+    
+    // Método para obtener la edad de la persona
     public int getEdad() {
         return edad;
     }
+    
+    // Método para establecer la edad de la persona
     public void setEdad(int edad) {
         this.edad = edad;
     }
+    
+    // Método para obtener la altura de la persona
     public double getAltura() {
         return altura;
     }
+    
+    // Método para establecer la altura de la persona
     public void setAltura(double altura) {
         this.altura = altura;
     }
+    
+    // Método toString para representar la información de la persona
     @Override
     public String toString() {
         return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", altura=" + altura + "]";
     }
 }
-    
-    
-
-
